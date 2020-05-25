ecd67e922d7617abe0cffd528ace3101e121e699
Yigit Boyar <yboyar@yboyar-macbookpro3.roam.corp.google.com>
2020-05-25 08:17:59 -0700

    test mkdir impl for mac

---
+++ ksqlite3/src/macTest/com/birbit/sqlite3/OsSpecificTestUtils.kt
@@ -0,0 +1,12 @@
+package com.birbit.sqlite3
+
+import platform.posix.S_IRUSR
+import platform.posix.S_IWUSR
+import platform.posix.S_IXUSR
+import platform.posix.mkdir
+
+actual object OsSpecificTestUtils{
+    internal actual fun mkdirForTest(path: String) {
+        mkdir(path, S_IRUSR.or(S_IWUSR).or(S_IXUSR).toUShort())
+    }
+}
