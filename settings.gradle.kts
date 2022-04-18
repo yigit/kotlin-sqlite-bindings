8dd7c4c4708f353fbed0438b91b3607b88dfbbc1
Yigit Boyar <yboyar@gmail.com>
2022-04-17 23:18:07 -0700

    use gradle action to setup gradle (#41)
    * use gradle action to setup gradle
    
    * WIP GCP cache
    
    * apply spotless, pass more gradle cache

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,11 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 pluginManagement {
     repositories {
         mavenCentral()
         maven("https://plugins.gradle.org/m2/")
         google()
+        maven("https://jitpack.io")
     }
     val spotlessVersion: String by settings
     val ktlintVersion: String by settings
@@ -27,7 +29,37 @@ pluginManagement {
         id("org.jlleitschuh.gradle.ktlint") version ktlintVersion
         kotlin("multiplatform") version kotlinVersion
         kotlin("jvm") version kotlinVersion
+        id("androidx.build.gradle.gcpbuildcache") version "a8ca1b978a"
     }
+    // TODO remove when that plugin is shipped to gradle plugin portal
+    this.resolutionStrategy.eachPlugin {
+        if (this.requested.id.id == "androidx.build.gradle.gcpbuildcache") {
+            this.useModule("com.github.yigit.gcp-gradle-build-cache:gcpbuildcache:a8ca1b978a")
+        }
+    }
+}
+
+plugins {
+    id("androidx.build.gradle.gcpbuildcache")
+}
+
+val gcpKey = providers.environmentVariable("GRADLE_CACHE_KEY").orNull
+    ?: providers.environmentVariable("GRADLE_CACHE_FILE").orNull?.let {
+        File(it).readText()
+    }
+val cacheIsPush = providers.environmentVariable("GRADLE_CACHE_PUSH").orNull?.toBoolean() ?: false
+if (gcpKey != null) {
+    println("setting up remote build cache with push: $cacheIsPush")
+    buildCache {
+        remote(androidx.build.gradle.gcpbuildcache.GcpBuildCache::class) {
+            projectId = "kotlin-sqlite-bindings"
+            bucketName = "kotlin-sqlite-bindings-cache"
+            credentials = androidx.build.gradle.gcpbuildcache.ExportedKeyGcpCredentialsText(gcpKey)
+            isPush = cacheIsPush
+        }
+    }
+} else {
+    println("not using remove build cache")
 }
 includeBuild("buildPlugin")
 include("sqlitebindings", "sqlitebindings-api", "jnigenerator", "ksqlite3")
